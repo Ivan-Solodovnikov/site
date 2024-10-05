c8028506847197fbb1f5e438971a85b3ada056c7
IvanSolodovnikov <130137738+Ivan-Solodovnikov@users.noreply.github.com>
2024-10-05 23:56:18 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html><html lang="en"><head>
+ <link href="https://vjs.zencdn.net/8.16.1/video-js.css" rel="stylesheet" />
+
+  <!-- If you'd like to support IE8 (for Video.js versions prior to v7) -->
+  <!-- <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script> -->
+ <title>Grass - Ваш взгляд на Интернет очень ценён. Получите за это деньги.</title>
+ <meta name="description" content="Получайте пассивный доход, продавая неиспользуемые сетевые ресурсы. Получайте пассивный доход за совместное использование вашего интернет-соединения.">
+ <meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<meta name="apple-itunes-app" content="">
+<meta name="apple-mobile-web-app-title" content="Grass Web">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name="theme-color" content="#08110B">
+
+<meta name="robots" content="index, follow">
+<meta name="keywords" content="грасс,интернет,заработок,grass, getgrass, getgrassru, kripto, crypto, cryptocurrency, mining, cloud mining, earn, income, passive income, зарабатывать, пассивный, доход, майнинг, selling unused network, selling your unused network resources">
+<meta name="revisit-after" content="7 days">
+<meta name="content-language" content="russian, rus, ru">
+<meta name="page-topic" content="зарабатывать, пассивный, доход, майнинг">
+<meta name="url" content="https://getgrass.ru">
+<meta name="copyright" content="Copyright (C) 2024">
+
+<meta property="og:url" content="https://getgrass.ru">
+<meta property="og:site_name" content="GetGrassru">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Grass - Ваш взгляд на Интернет очень ценен. Получите за это деньги. GetGrass.ru">
+<meta property="og:description" content="Получайте пассивный доход, продавая неиспользуемые сетевые ресурсы. Получайте пассивный доход за совместное использование вашего интернет-соединения.">
+<meta property="og:image" content="images/apple-touch-icon.png">
+<meta property="og:image:url" content="images/apple-touch-icon.png">
+<meta property="og:image:secure_url" content="images/apple-touch-icon.png">
+<meta property="og:image:alt" content="Grass - Ваш взгляд на Интернет очень ценён. Получите за это деньги. GetGrass.ru">
+
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@getgrass">
+<meta name="twitter:url" content="https://getgrass.ru">
+<meta name="twitter:domain" content="https://getgrass.ru">
+<meta name="twitter:title" content="Grass - Ваш взгляд на Интернет очень ценён. Получите за это деньги. GetGrass.ru">
+<meta name="twitter:description" content="Получайте пассивный доход, продавая неиспользуемые сетевые ресурсы. Получайте пассивный доход за совместное использование вашего интернет-соединения.">
+<meta name="twitter:image" content="https://getgrass.ru/assets/img/apple-touch-icon.png">
+<meta name="twitter:image:alt" content="Получайте пассивный доход, продавая неиспользуемые сетевые ресурсы. Получайте пассивный доход за совместное использование вашего интернет-соединения.">
+
+<link rel="canonical" href="https://getgrass.ru">
+
+<!-- Favicons -->
+<link href="favicon.ico" rel="icon">
+<link href="images/apple-touch-icon.png" rel="apple-touch-icon">
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+<!-- Vendor CSS Files -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/icofont.min.css" rel="stylesheet">
+<link href="css/boxicons.min.css" rel="stylesheet">
+<link href="css/venobox.css" rel="stylesheet">
+<link href="css/owl.carousel.min.css" rel="stylesheet">
+<link href="css/aos.css" rel="stylesheet">
+
+<!-- Template Main CSS File -->
+<link href="css/style.css" rel="stylesheet">
+
+<!-- Google tag (gtag.js) -->
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=G-FH3VPZJWH6"></script>
+<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-FH3VPZJWH6');</script>
+
+</head>
+
+<body>
+
+    <!-- ======= Top Bar ======= -->
+  <div id="topbar" class="d-none d-lg-flex align-items-center fixed-top">
+    <div class="container d-flex">
+      <div class="contact-info mr-auto">
+        <a href="https://chrome.google.com/webstore/detail/grass-extension/ilehaonighjijnmpnagapkhpcdbhclfg" target="_blank"><i class="icofont-download"></i> Скачать расширение Chrome</a>
+      </div>
+	  <div class="social-links">
+        <a href="https://twitter.com/getgrass_io" target="_blank" class="twitter" title="Grass on Twitter"><i class="icofont-twitter"></i></a>
+        <a href="http://www.bitsypool.com/?r=1NvZZSAWGPVJjRSW8RKb7oZXXQi6sjf2yj" target="_blank" class="discord" title="Browser Mining from FaucetPay"><i class="icofont-discord"></i></a>
+	  </div>
+    </div>
+  </div>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+	  <h1 class="logo mr-auto d-none"><a href="/" title="Grass - Ваш взгляд на Интернет очень ценен. Получите за это деньги.">Grass</a></h1>
+	  <a href="/" class="logo mr-auto" title="Grass - Ваш взгляд на Интернет очень ценен. Получите за это деньги.">
+		<svg width="160" height="auto" viewBox="0 0 122 34" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.1659 14.3738C19.1659 11.3075 16.6802 8.82178 13.6139 8.82178V27.3269C13.6139 30.3932 16.0996 32.8789 19.1659 32.8789V14.3738Z" fill="#A9D4B6"></path><path d="M7.37631 19.2629C7.37631 16.1967 9.86203 13.7109 12.9283 13.7109V25.5358C12.9283 28.6021 10.4426 31.0878 7.37631 31.0878V19.2629Z" fill="#A9D4B6"></path><path d="M19.8513 8.89296C19.8513 5.82667 22.3371 3.34094 25.4034 3.34094V22.3112C25.4034 25.3775 22.9176 27.8632 19.8513 27.8632V8.89296Z" fill="#A9D4B6"></path><path d="M25.9749 3.29524C25.9749 1.7628 27.2172 0.520508 28.7497 0.520508C30.2821 0.520508 31.5244 1.7628 31.5244 3.29524C31.5244 4.82769 30.2821 6.06998 28.7497 6.06998C27.2172 6.06998 25.9749 4.82769 25.9749 3.29524Z" fill="#A9D4B6"></path><path d="M1.1344 28.6956C1.1344 25.6293 3.62012 23.1436 6.68641 23.1436V27.3269C6.68641 30.3932 4.20069 32.8789 1.1344 32.8789V28.6956Z" fill="#A9D4B6"></path><path d="M0.300934 30.8587C17.4277 30.8587 13.3059 3.23615 31.8002 3.23615" stroke="#012E0F" stroke-width="0.221817"></path><path d="M57.1559 5.38697V7.61623C56.1245 6.15224 54.0616 5.12079 51.8323 5.12079C46.7083 5.12079 43.8136 8.78077 43.8136 13.7717C43.8136 18.7625 46.7083 22.4225 51.8656 22.4225C54.2279 22.4225 56.3574 21.4576 57.1559 20.0269V22.7553C57.1559 24.7849 55.7585 26.2489 53.8286 26.2489H48.7712C47.2828 26.2489 46.0761 27.4555 46.0761 28.944H54.1614C57.8214 28.944 60.3833 26.4818 60.3833 22.9549V5.38697H57.1559ZM52.2316 19.5944C49.1372 19.5944 47.1076 17.2986 47.1076 13.7717C47.1076 10.2448 49.1372 7.94896 52.2316 7.94896C55.3592 7.94896 57.3888 10.2448 57.3888 13.7717C57.3888 17.2986 55.3592 19.5944 52.2316 19.5944Z" fill="#A9D4B6"></path><path d="M74.3433 8.18187V5.38697H72.3136C69.9513 5.38697 68.6204 6.38515 67.8551 8.61441V5.38697H64.6277V22.1564H67.8551V14.5702C67.8551 10.9102 68.8533 8.18187 71.9144 8.18187H74.3433Z" fill="#A9D4B6"></path><path d="M83.043 5.12079C79.0836 5.12079 76.089 7.58296 75.8229 10.8769H78.8174C78.9172 9.18004 80.6141 7.71605 82.9432 7.71605C85.5385 7.71605 87.0024 9.1135 87.0024 11.1431C87.0024 11.742 86.6364 12.1413 85.8712 12.1413H82.3776C78.4181 12.1413 75.8561 14.1377 75.8561 17.3651C75.8561 20.3596 78.1187 22.4225 81.5457 22.4225C84.0745 22.4225 86.2039 21.2247 87.0024 19.428V22.1564H90.2299V11.3428C90.2299 7.61623 87.3684 5.12079 83.043 5.12079ZM82.1779 19.8605C80.3147 19.8605 79.0503 18.8291 79.0503 17.2986C79.0503 15.5351 80.481 14.4038 82.677 14.4038H87.0024V14.8031C87.0024 17.8975 85.239 19.8605 82.1779 19.8605Z" fill="#A9D4B6"></path><path d="M99.8579 22.4225C103.651 22.4225 106.146 20.4594 106.146 17.3984C106.146 9.74568 96.5639 14.0046 96.5639 9.8455C96.5639 8.54787 97.7284 7.68278 99.4586 7.68278C100.889 7.68278 102.753 8.44805 102.986 10.3779H106.013C105.847 7.21696 103.252 5.12079 99.4586 5.12079C95.965 5.12079 93.5028 7.0506 93.5028 9.94531C93.5028 17.0989 103.019 13.0064 103.019 17.4649C103.019 18.696 101.788 19.7275 99.8579 19.7275C97.6952 19.7275 96.2977 18.6295 96.0981 16.6996H93.0703C93.3032 20.2265 95.9317 22.4225 99.8579 22.4225Z" fill="#A9D4B6"></path><path d="M115.389 22.4225C119.182 22.4225 121.678 20.4594 121.678 17.3984C121.678 9.74568 112.095 14.0046 112.095 9.8455C112.095 8.54787 113.26 7.68278 114.99 7.68278C116.421 7.68278 118.284 8.44805 118.517 10.3779H121.545C121.378 7.21696 118.783 5.12079 114.99 5.12079C111.497 5.12079 109.034 7.0506 109.034 9.94531C109.034 17.0989 118.55 13.0064 118.55 17.4649C118.55 18.696 117.319 19.7275 115.389 19.7275C113.227 19.7275 111.829 18.6295 111.63 16.6996H108.602C108.835 20.2265 111.463 22.4225 115.389 22.4225Z" fill="#A9D4B6"></path></svg>
+	  </a>
+      <a href="https://app.getgrass.io/register/?referralCode=J3UApEBTZ6teJzN" target="_blank" class="btn-menu">Скачать<span> Grass</span></a>
+      <a href="https://app.gradient.network/signup?code=BD6S7D" target="_blank" class="btn-menu"><span>Скачать </span>Gradient</a>
+    </div>
+  </header><!-- End Header -->
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+    <div class="container position-relative" data-aos="fade-down" data-aos-delay="500">
+
+      <div class="row">
+        <div class="col-lg-6 order-2 order-lg-1 pdesktop" data-aos="fade-right" data-aos-delay="500">
+			<h1>Ваш взгляд на Интернет очень ценён. Получите за это деньги.</h1>
+			<h2>Получайте пассивный доход, продавая неиспользуемые сетевые ресурсы.</h2>
+			<a href="https://app.getgrass.io/register/?referralCode=J3UApEBTZ6teJzN" target="_blank" class="btn-get-started">Скачать Grass</a>
+			<a href="https://app.gradient.network/signup?code=BD6S7D" target="_blank" class="btn-get-started">Скачать Gradient</a>
+		</div>
+        <div id="live" class="col-lg-6 order-1 order-lg-2 pb-3 text-center" data-aos="fade-left" data-aos-delay="500">
+          <img src="images/hero-banner.png" class="img-fluid" alt="Получайте пассивный доход, продавая неиспользуемые сетевые ресурсы. Получайте пассивный доход за совместное использование вашего интернет-соединения.">
+<video
+    id="my-video"
+    class="video-js"
+    controls
+    preload="auto"
+    width="466"
+    height="263"
+    poster="https://i.postimg.cc/QtK610TX/6f76f45ea39008ee4362f9d4e726deed.jpg"
+    data-setup="{}"
+  >
+    
+	<source src="https://ivan-solodovnikov.narod.ru/videoplayer/media/Golden_Earring-Going_To_The_Run.mp4" type="video/mp4" />
+    
+    <p class="vjs-no-js">
+      To view this video please enable JavaScript, and consider upgrading to a
+      web browser that
+      <a href="https://videojs.com/html5-video-support/" target="_blank"
+        >supports HTML5 video</a
+      >
+    </p>
+  </video>
+        </div>
+      </div>
+
+	</div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section class="about">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-down" data-aos-delay="0">
+          <h1>Начни зарабатывать в 3 шага</h1>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="150">
+            <a href="https://app.getgrass.io/register/?referralCode=J3UApEBTZ6teJzN" target="_blank">
+              <div class="box">
+                <p><i class="icofont-download"></i></p>
+                <span>01.</span>
+                <h4>Создать аккаунт</h4>
+                <p>Войдите в свою учетную запись Grass в расширении от Google Chrome.</p>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="300">
+            <a href="https://chrome.google.com/webstore/detail/grass-extension/ilehaonighjijnmpnagapkhpcdbhclfg" target="_blank">
+            <div class="box">
+              <p><i class="icofont-power"></i></p>
+              <span>02.</span>
+              <h4>Скачать</h4>
+              <p>Загрузите веб-расширение Grass от интернет-магазина Google Chrome.</p>
+            </div>
+            </a>
+		  </div>
+          <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="450">
+            <a href="https://app.getgrass.io/register/?referralCode=J3UApEBTZ6teJzN" target="_blank">
+            <div class="box">
+              <p><i class="icofont-dollar"></i></p>
+              <span>03.</span>
+              <h4>Зарабатывайте</h4>
+              <p>Grass работает в фоновом режиме. Не отвлекает вас от других дел.</p>
+            </div>
+            </a>
+		  </div>
+        </div>
+
+        <div class="section-bottom" data-aos="fade-up" data-aos-delay="500">
+          <h2>Получайте пассивный доход за то, что делитесь своим интернет-соединением.</h2>
+        </div>
+
+      </div>
+    </section>
+	<!-- End About Section -->
+
+  </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container text-center">
+        <div class="row">
+
+          <div class="col-12 pb-4">
+            <p>
+              <a href="/" class="logo mr-auto"><img src="images/grass-logo-large.svg" alt="" class="img-fluid"></a>
+			</p>
+          </div>
+
+		  <div class="col-12 pb-4">
+            <a href="https://app.getgrass.io/register/?referralCode=J3UApEBTZ6teJzN" target="_blank" class="btn-main"><i class="icofont-download"></i> Скачать Grass</a>
+            <a href="https://app.gradient.network/signup?code=BD6S7D" target="_blank" class="btn-main"><i class="icofont-twitter"></i>Скачать Gradient</a>
+            <a href="http://www.bitsypool.com/?r=1NvZZSAWGPVJjRSW8RKb7oZXXQi6sjf2yj" target="_blank" class="btn-main"><i class="icofont-discord"></i>Browser Mining from FaucetPay</a>
+          </div>
+
+          <div class="col-12 footer-notice">
+			<p>
+			  <strong>ВАЖНО:</strong><br>Присоединяясь к Grass, вы соглашаетесь с тем, что понимаете, что рынок криптовалют является нестабильным рынком.<br>Если вы зарабатываете какое-либо вознаграждение или любой другой токен, вы понимаете, что в любое время Стоимость заработанного токена может быть больше или меньше, чем усилия, которые вы приложили для его получения.
+<br>Команда Grass оставляет за собой право увеличивать или уменьшать вознаграждения по своему усмотрению.
+			</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="copyright">
+        © 2024 GetGrass.ru      </div>
+    </div>
+    <div class="container-fluid bg">
+      <div class="credits">
+		<a href="https://www.getgrass.io/privacy-policy" target="_blank">Политика конфиденциальности</a> ·
+<a href="https://www.getgrass.io/terms-and-conditions" target="_blank">Правила и условия</a> ·
+<a href="https://www.getgrass.io/blog" target="_blank">Блог</a> ·
+<a href="https://wynd-network.gitbook.io/grass/info/faq" target="_blank">Часто задаваемые вопросы</a> ·
+<a href="http://www.bitsypool.com/?r=1NvZZSAWGPVJjRSW8RKb7oZXXQi6sjf2yj" target="_blank">Поддержка</a>
+	  </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <!-- Vendor JS Files -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/isotope.pkgd.min.js"></script>
+<script src="js/venobox.min.js"></script>
+<script src="js/jquery.waypoints.min.js"></script>
+<script src="js/counterup.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/aos.js"></script>
+
+<!-- Template Main JS File -->
+<script src="js/main.js"></script>
+  <!-- Add to Home Screen -->
+<link href="css/add2home.css" rel="stylesheet">
+<script src="js/add2home.js"></script>
+<script src="https://vjs.zencdn.net/8.16.1/video.min.js"></script>
+</body></html>
